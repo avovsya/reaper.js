960fba41e51838c282ae667f31c2f006f11897a5
Artem Vovsia <avovsya@gmail.com>
2015-12-19 00:43:42 +0000

    First prototype of the extractor. Not tested

---
+++ test/reaper.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+var reaper = require('../lib/reaper');
+
+describe('reaper.js', function() {
+  it('should export extract function', function() {
+    expect(reaper.extract).to.exist;
+  });
+
+  it('', function() {
+
+  });
+});
